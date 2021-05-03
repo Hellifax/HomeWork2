a1b8081604801d81f9bf0bd402b190ded4aaee01
Hellifax <hellifax91@gmail.com>
2021-05-03 22:34:30 +0300

    Task 4

---
+++ HW2.3/2.3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adress = "Туристическая стоянка";
+            string adress1 = "г. Севастополь";
+            string adress2 = "Мыс Сарыч";
+            var ID = 000037654162;
+            var FN = 2703005107;
+            string zone = "Место отдыха зона 3";
+            string price = "8 x 30.00           240р";
+            int sum = 240;
+            var date = "01-05-2013     18:32";
+            Console.WriteLine("ООО \"Туризм, охота, рыбалка\"");
+            Console.WriteLine("г. Севастополь");
+            Console.WriteLine("Мыс Сарыч");
+            Console.WriteLine(ID);
+            Console.WriteLine(FN);
+            Console.WriteLine("Место отдыха зона 3");
+            Console.WriteLine("8 x 30.00           240р");
+            Console.WriteLine(sum);
+            Console.WriteLine(date);
+
+
+        }
+    }
+}
