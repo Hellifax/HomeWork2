1cde62766bfe07834dd5a37871df910d2020e521
Hellifax <hellifax91@gmail.com>
2021-05-03 22:34:03 +0300

    Task 3

---
+++ HW2.2/2.2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            var a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Вы ввели четное число");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели нечетное число");
+            }
+        }
+    }
+}
